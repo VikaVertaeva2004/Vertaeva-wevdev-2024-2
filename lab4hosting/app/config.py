4e434f9455919fa2444d1759fa67de67a12c654e
sdjk <113758923+VikaVertaeva2004@users.noreply.github.com>
2024-06-12 13:57:41 +0300

    Delete lab4hosting/app directory

--- lab4hosting/app/config.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY = 'f038a541489b89f81762d12edfdd03835ceea10cfb3cdbdabfbfa0f48b0d4802'
-MYSQL_USER = 'std_2639_vert'
-MYSQL_PASSWORD = '12345678'
-MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
-MYSQL_DATABASE = 'std_2639_vert'
